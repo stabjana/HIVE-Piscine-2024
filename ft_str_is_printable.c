3348bfa8b7811cc5397b32c46544bbad0c84ed1a
stabjana <162564271+stabjana@users.noreply.github.com>
2024-03-07 12:32:18 +0200

    added exercise C02

---
+++ ft_str_is_printable.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sjana <sjana@student.hive.fi>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/21 15:51:51 by sjana             #+#    #+#             */
+/*   Updated: 2024/02/25 14:04:06 by sjana            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_str_is_printable(char *str)
+
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '\0')
+	{
+		return (1);
+	}
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 32 && str[i] <= 127)
+		{
+			i++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+int	main(void)
+{
+	char	str[] = "€";
+	printf("String : %s\nResult : %d\n", str, ft_str_is_printable(str));
+	return 0;
+}
